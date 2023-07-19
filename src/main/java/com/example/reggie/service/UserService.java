062d8cd6541f615f40bd9ccac5fbc63cdccdf742
Junwei0151 <102798557+JunweiZhangOrSteven@users.noreply.github.com>
2023-07-19 12:52:30 +0800

    Delete src directory

--- src/main/java/com/example/reggie/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.example.reggie.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.reggie.entity.User;
-
-public interface UserService extends IService<User> {
-}
--- src/test/java/com/example/reggie/service/EmployeeService.java
+++
@@ -1,7 +0,0 @@
-package com.example.reggie.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.reggie.entity.Employee;
-
-public interface EmployeeService extends IService<Employee> {
-}
